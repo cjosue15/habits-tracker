f776e56057ec9cccade964be8cf45412882ead9e
Josue Morales <dev.carlos.morales@gmail.com>
2024-02-16 20:07:22 -0500

    Config add animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,25 @@ const config: Config = {
       boxShadow: {
         appShadow: "0px 8px 16px #000c,0px 0px 1px inset #ffffff3b",
       },
+      animation: {
+        "dialog-in": "slide-in .4s cubic-bezier(.05, .7, .1, 1)",
+        "dialog-out": "slide-out .2s cubic-bezier(.3, 0, .8, .15)",
+        "backdrop-in": "fade-in .25s cubic-bezier(.05, .7, .1, 1)",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": { transform: "translateY(64px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        "slide-out": {
+          "0%": { transform: "translateY(0)", opacity: "1" },
+          "100%": { transform: "translateY(64px)", opacity: "0" },
+        },
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
     },
   },
   plugins: [],
