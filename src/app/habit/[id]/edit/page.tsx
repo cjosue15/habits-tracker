67a15b968de04157d8e522c5d1df5e2994e3ad97
Josue Morales <dev.carlos.morales@gmail.com>
2024-03-24 14:23:45 -0500

    just test hours

--- src/app/habit/[id]/edit/page.tsx
+++ src/app/habit/[id]/edit/page.tsx
@@ -2,6 +2,9 @@ import HabitForm from "@/app/habit/ui/HabitForm";
 import ContainerForm from "@/components/ContainerForm";
 
 export default function HabitEditPage({ params }: { params: any }) {
+  console.log(new Date());
+  console.log(new Date().getHours());
+  console.log(new Date().getUTCHours());
   return (
     <ContainerForm>
       <HabitForm id={params.id} />
